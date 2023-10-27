0f273f3b4a57b5d595ccdb3839b5ff5117b9271b
Roman Burkard <roman.burkard@gmail.com>
2023-10-27 11:34:29 +0200

    add

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,14 @@ export default function Home() {
 		<main className="flex min-h-screen flex-col items-center justify-center p-24 relative overflow-hidden">
 			<div className="flex relative  w-screen md:w-2/3 lg:w-1/2">
 				<video
-					className="z-10 transform -rotate-12 transition ease-in-out max-w-full"
+					className="transform -rotate-12 transition ease-in-out max-w-full"
 					src={'/intro-text.mov'}
 					autoPlay
 					playsInline
 					muted
 				/>
 			</div>
-			<div className="flex relative justify-center items-center w-screen md:w-2/3 lg:w-1/2 -mt-24">
+			<div className="flex relative justify-center items-center w-screen md:w-2/3 lg:w-1/2 -mt-24 z-10">
 				<Image
 					className=""
 					width={300}
