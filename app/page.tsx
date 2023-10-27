e06ad90d9590514e4461ddde502b473152419c99
Roman Burkard <roman.burkard@gmail.com>
2023-10-27 11:32:23 +0200

    add roman

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,9 @@ import Image from 'next/image';
 import {useEffect, useRef, useState} from 'react';
 
 export default function Home() {
-	const videoRef = useRef<HTMLVideoElement>(null);
-
-	const [hover, setHover] = useState(false);
-
-	useEffect(() => {
-		if (hover && videoRef.current) {
-			videoRef.current.play();
-		}
-	}, [hover]);
-
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center p-24 relative overflow-hidden">
-			<div
-				className="flex relative md:w-1/3 md:left-1 cursor-pointer"
-				onMouseEnter={() => setHover(true)}
-				onMouseLeave={() => setHover(false)}
-			>
+			<div className="flex relative  w-screen md:w-2/3 lg:w-1/2">
 				<video
 					className="z-10 transform -rotate-12 transition ease-in-out max-w-full"
 					src={'/intro-text.mov'}
@@ -27,23 +13,13 @@ export default function Home() {
 					playsInline
 					muted
 				/>
-				{hover && (
-					<video
-						ref={videoRef}
-						className="z-10 transform -rotate-12 transition ease-in-out absolute left-2/3 top-1/4 w-1/4"
-						src={'/select.mov'}
-						width={100}
-						playsInline
-						muted
-					/>
-				)}
 			</div>
-			<div className="flex items-center justify-center animate-pulse forward">
+			<div className="flex relative justify-center items-center w-screen md:w-2/3 lg:w-1/2 -mt-24">
 				<Image
-					className="transform -scale-y-100"
+					className=""
 					width={300}
 					height={300}
-					src={'/macbook.png'}
+					src={'/roman-on-floor.png'}
 					alt={'macbook lying on the floor'}
 				/>
 			</div>
