b7a17ce50115824a8f87329fcd540dabc27e35a3
Roman Burkard <roman.burkard@gmail.com>
2023-11-22 15:27:52 +0100

    wip

---
+++ app/home-dashboard/page.tsx
@@ -0,0 +1,66 @@
+'use client';
+import Image from 'next/image';
+import {Button} from '../../components/button';
+import {generateTags} from '@/functions/generateTags';
+import {useState} from 'react';
+import {addDays} from 'date-fns';
+
+const convertDate = (date: Date) => {
+	const today = date;
+	return `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`; // yyyy-mm-dd
+};
+
+export default function HomeDashboard() {
+	const [day, setDay] = useState<Array<string>>([]);
+	const url =
+		'https://project-server-6p2zykct3q-ew.a.run.app/dashboard-adding-task';
+
+	const [task, setTask] = useState('');
+	const [date, setDate] = useState(new Date());
+
+	const [disabled, setDisabled] = useState(false);
+
+	const submit = async (event: React.FormEvent<HTMLButtonElement>) => {
+		event.preventDefault();
+		setDisabled(true);
+
+		const response = await fetch(url, {
+			method: 'POST', // *GET, POST, PUT, DELETE, etc.
+			headers: {
+				'Content-Type': 'application/json',
+				'x-api-key': String(process.env.NEXT_PUBLIC_API_KEY),
+				// 'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			body: JSON.stringify({date, task}), // body data type must match "Content-Type" header
+		});
+
+		setDisabled(false);
+	};
+
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-center gap-8 p-24">
+			<input
+				className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-60 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				value={task}
+				placeholder="Task decription"
+				maxLength={10}
+				onChange={(e) => {
+					setTask(e.currentTarget.value);
+				}}
+			/>
+			<div className="flex gap-4 justify-center items-center">
+				<p>{`${date.getDate()}.${
+					date.getMonth() + 1
+				}.${date.getFullYear()}`}</p>
+				<Button
+					title={'+'}
+					onClick={() => {
+						const newDate = addDays(date, 1);
+						return setDate(newDate);
+					}}
+				/>
+			</div>
+			<Button title={'Add Task'} onClick={submit} />
+		</main>
+	);
+}
